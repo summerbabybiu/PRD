e518e01d06ad952e71f4c978dfc12c0e2d8ce1ec
米珐 <stephenwzl.wzl@antfin.com>
2017-09-29 16:08:29 +0800

    add comment

--- router/post.js
+++ router/post.js
@@ -7,6 +7,7 @@ const router = require('express').Router(),
       pagination = require('../middleware').pagination,
       mono = require('../lib/mono')
 
+// TODO: 这个数据库驱动貌似不支持 skip等 API，所以分页先做个样子，实际一次查询是返回所有的
 router.get('/list', pagination, (req, res) => {
   mono.queryPost(req.query.page, req.query.limit)
     .then(results => {
@@ -17,6 +18,9 @@ router.get('/list', pagination, (req, res) => {
     })
 });
 
+// TODO: 创建文章要求登录，从cookie取 userid写成了中间件，去看一下就可以了
+// @param: title 文章标题
+// @param: content  markdown格式的文章内容
 router.post('/create', loginRequire, (req, res) => {
   let title = req.body.title
   let content = req.body.content
@@ -38,6 +42,11 @@ router.post('/create', loginRequire, (req, res) => {
     })
 });
 
+// TODO: 更新文章只允许更新 title 和 content
+// @param: postid 文章id
+// @param: title 文章标题 可选
+// @param: content 文章内容 可选
+// title 和 content 至少传一个
 router.post('/update', loginRequire, (req, res) => {
   let title = req.body.title
   let content = req.body.content
@@ -57,6 +66,7 @@ router.post('/update', loginRequire, (req, res) => {
     })
 })
 
+// TODO: 删除文章只需要传文章 id
 router.post('/delete', loginRequire, (req, res) => {
   let postid = req.body.postid
   if (!utils.paramValidator(postid)) {
