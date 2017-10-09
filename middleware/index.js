f6ab13d3dbba7386f9aa73749182d008502850f7
米珐 <stephenwzl.wzl@antfin.com>
2017-10-09 11:26:46 +0800

    remove mono

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,4 @@
-const mono = require('../lib/mono'),
-      util = require('../lib/utils')
+const util = require('../lib/utils');
 
 // 这是一个中间件函数，目的在真的处理一个请求前校验一些信息
 // 比如用户的 sessionToken是否合法，对应的 userid是谁
@@ -18,7 +17,7 @@ exports.requireLogin = function (req, res, next) {
 exports.pagination = function (req, res, next) {
   var page = req.query.page
   var limit = req.query.limit
-  if (!util.paramValidator(page)) {
+  if (!util.paramValidator(page) || page == 0) {
     req.query.page = 1
   }
   if (!util.paramValidator(limit)) {
