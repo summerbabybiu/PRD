5ce8ba5f69474ffc027dafcfcd5b8222e7681a6b
米珐 <stephenwzl.wzl@antfin.com>
2017-10-09 11:25:33 +0800

    remove mongolass

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
-const Mongolass = require('mongolass');
-const mongolass = new Mongolass();
-mongolass.connect('mongodb://localhost:27017/test');// const mongolass = new Mongolass('mongodb://localhost:27017/test');
-
 const ParseServer = require('parse-server').ParseServer;
 const config = require('./lib/config');
 
